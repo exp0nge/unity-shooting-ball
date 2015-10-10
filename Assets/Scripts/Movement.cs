ad5243871dbd704ea00d97a85f7231e550804730
exp0nge <mdislamwork@gmail.com>
2015-10-09 22:56:19 -0400

    Add basic files for rolling ball.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float speed = 0.1F;
+	public float gravity = -999.81F;
+	private CharacterController charCtrl;
+	//public float gravity = -20.5F;
+	private Vector3 direction = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		charCtrl = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move();
+    }
+	private void move() {
+		transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+		direction = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * speed;
+		charCtrl.Move(direction);
+	}
+}
